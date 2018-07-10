639cbda08b43d1a5393a30103febe58382150848
FarPoint <hgao61@gatech.edu>
2018-07-10 00:51:34 -0700

    Update RavensAttribute.py

--- RavensAttribute.py
+++ RavensAttribute.py
@@ -1,15 +1,7 @@
-# DO NOT MODIFY THIS FILE.
-#
-# When you submit your project, an alternate version of this file will be used
-# to test your code against the sample Raven's problems in this zip file, as
-# well as other problems from the Raven's Test and former students.
-#
-# Any modifications to this file will not be used when grading your project.
-# If you have any questions, please email the TAs.
-
 # A single variable-value pair that describes some element of a RavensObject.
 # For example, a circle might have the attributes shape:circle, size:large, and
 # filled:no.
+
 class RavensAttribute:
     # Creates a new RavensAttribute.
     #
